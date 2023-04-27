e81d55647405bf57aed4a5d85b2590092638c56d
FalkoKa <115454465+FalkoKa@users.noreply.github.com>
2023-04-27 12:44:47 +0700

    put serach from nav in separate component

--- src/components/Categories_nav/Categories_nav.js
+++ src/components/Categories_nav/Categories_nav.js
@@ -1,50 +1,6 @@
 import { Link } from 'react-router-dom';
 import './Categories_nav.css';
-import SearchIcon from '@mui/icons-material/Search';
-import { styled, alpha } from '@mui/material/styles';
-import InputBase from '@mui/material/InputBase';
-
-const Search = styled('div')(({ theme }) => ({
-  position: 'relative',
-  borderRadius: theme.shape.borderRadius,
-  backgroundColor: alpha(theme.palette.common.white, 0.15),
-  '&:hover': {
-    backgroundColor: alpha(theme.palette.common.white, 0.25),
-  },
-  marginLeft: 0,
-  width: '100%',
-  [theme.breakpoints.up('sm')]: {
-    marginLeft: theme.spacing(1),
-    width: 'auto',
-  },
-}));
-
-const SearchIconWrapper = styled('div')(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  height: '100%',
-  position: 'absolute',
-  pointerEvents: 'none',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-}));
-
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: 'inherit',
-  '& .MuiInputBase-input': {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      width: '12ch',
-      '&:focus': {
-        width: '20ch',
-      },
-    },
-  },
-}));
+import Search from '../Search';
 
 export default function CategoriesNav(props) {
   return (
@@ -54,22 +10,11 @@ export default function CategoriesNav(props) {
         <Link>Clothing</Link>
         <Link>Shoes</Link>
         <Link>Sports</Link>
-        <Link>Streetwear</Link>
+        <Link>Brands</Link>
         <Link>Accessories</Link>
         <Link>Sale %</Link>
       </nav>
-      <div className="search-input">
-        {/* <input type="search" placeholder="Search" /> */}
-        <Search>
-          <SearchIconWrapper>
-            <SearchIcon />
-          </SearchIconWrapper>
-          <StyledInputBase
-            placeholder="Search…"
-            inputProps={{ 'aria-label': 'search' }}
-          />
-        </Search>
-      </div>
+      <Search />
     </div>
   );
 }
