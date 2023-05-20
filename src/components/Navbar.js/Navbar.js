ba5f9fb2834131978afa68b456440572465fd5ad
FalkoKa <115454465+FalkoKa@users.noreply.github.com>
2023-05-20 14:36:17 +0700

    layout drowdown menu

--- src/components/Navbar.js/Navbar.js
+++ src/components/Navbar.js/Navbar.js
@@ -9,6 +9,20 @@ import Badge from '@mui/material/Badge';
 export default function Navbar(props) {
   const { cart, user, logout } = useContext(userContext);
 
+  const subtotal = () => {
+    return cart
+      .map((i) => {
+        return { price: i.item.price, quantity: i.quantity };
+      })
+      .reduce((total, i) => {
+        return (total += i.price * i.quantity);
+      }, 0);
+  };
+
+  const deliveryFee = subtotal() > 49.0 ? 0 : 4.99;
+
+  subtotal();
+
   return (
     <div className="nav container-width-85">
       <Logo />
@@ -26,6 +40,25 @@ export default function Navbar(props) {
           </Badge>
         </Link>
       </nav>
+      <div className="dropdown-menu">
+        <h3>Your Cart</h3>
+        <div className="dropdwown-cart"></div>
+        <div className="dropdown-summary">
+          <div className="order-total">
+            <div className="flex">
+              <span>Delivery</span>
+              <span>{deliveryFee.toFixed(2)} USD</span>
+            </div>
+            <div className="flex">
+              <span>Total</span>
+              <span>{(subtotal() + deliveryFee).toFixed(2)} USD</span>
+            </div>
+            <div className="cart-link">
+              <Link to={'/cart'}>Go to cart</Link>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
