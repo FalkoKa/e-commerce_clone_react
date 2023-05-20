fd054e8018750c305e7d189324a1d056eba374f9
FalkoKa <115454465+FalkoKa@users.noreply.github.com>
2023-05-20 19:27:50 +0700

    hover dropdown

---
+++ src/components/DropdownMenu.js
@@ -0,0 +1,52 @@
+import DropdownItem from './DropdownItem';
+import { userContext } from '../userConext';
+import { useContext } from 'react';
+import { Link } from 'react-router-dom';
+
+export default function DropdownMenu() {
+  const { cart } = useContext(userContext);
+
+  const subtotal = () => {
+    return cart
+      .map((i) => {
+        return { price: i.item.price, quantity: i.quantity };
+      })
+      .reduce((total, i) => {
+        return (total += i.price * i.quantity);
+      }, 0);
+  };
+
+  const deliveryFee = subtotal() > 49.0 ? 0 : 4.99;
+
+  subtotal();
+
+  return (
+    <div className="dropdown-menu">
+      <h3>Your Cart</h3>
+      <div className="dropdwown-cart">
+        {cart.map((item) => (
+          <DropdownItem
+            item={item.item}
+            quantity={item.quantity}
+            key={item._id}
+          />
+        ))}
+      </div>
+      <div className="dropdown-summary">
+        <div className="order-total">
+          <div className="flex">
+            <span>Delivery</span>
+            <span>{deliveryFee.toFixed(2)} USD</span>
+          </div>
+          <div className="flex">
+            <span>Total</span>
+            <span>{(subtotal() + deliveryFee).toFixed(2)} USD</span>
+          </div>
+          <div className="cart-link">
+            <Link to={'/cart'}>Go to cart</Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/components/Navbar.js/Navbar.js
+++ src/components/Navbar.js/Navbar.js
@@ -1,73 +1,35 @@
 import { Link } from 'react-router-dom';
 import Logo from '../Logo/Logo';
-import DropdownItem from '../DropdownItem';
+import DropdownMenu from '../DropdownMenu';
 import './Navbar.css';
 import { CiUser, CiHeart, CiShoppingCart } from 'react-icons/ci';
 import { userContext } from '../../userConext';
-import { useContext, useState } from 'react';
+import { useContext } from 'react';
 import Badge from '@mui/material/Badge';
 
 export default function Navbar(props) {
   const { cart, user, logout } = useContext(userContext);
-
-  const subtotal = () => {
-    return cart
-      .map((i) => {
-        return { price: i.item.price, quantity: i.quantity };
-      })
-      .reduce((total, i) => {
-        return (total += i.price * i.quantity);
-      }, 0);
-  };
-
-  const deliveryFee = subtotal() > 49.0 ? 0 : 4.99;
-
-  subtotal();
 
   return (
     <div className="nav container-width-85">
       <Logo />
       <nav>
         {user && <button onClick={() => logout()}>Logout</button>}
-        <Link to={'/myaccount'}>
+        <Link className="navlink" to={'/myaccount'}>
           <CiUser size={25} />
         </Link>
-        <Link to={''}>
+        <Link className="navlink" to={''}>
           <CiHeart size={25} />
         </Link>
-        <Link className="cart-icon" to={'/cart'}>
-          <Badge color="secondary" badgeContent={cart.length}>
-            <CiShoppingCart size={25} />
-          </Badge>
-        </Link>
+        <div className="drop navlink">
+          <Link className="cart-icon" to={'/cart'}>
+            <Badge color="secondary" badgeContent={cart.length}>
+              <CiShoppingCart size={25} />
+            </Badge>
+          </Link>
+          <DropdownMenu />
+        </div>
       </nav>
-      <div className="dropdown-menu">
-        <h3>Your Cart</h3>
-        <div className="dropdwown-cart">
-          {cart.map((item) => (
-            <DropdownItem
-              item={item.item}
-              quantity={item.quantity}
-              key={item._id}
-            />
-          ))}
-        </div>
-        <div className="dropdown-summary">
-          <div className="order-total">
-            <div className="flex">
-              <span>Delivery</span>
-              <span>{deliveryFee.toFixed(2)} USD</span>
-            </div>
-            <div className="flex">
-              <span>Total</span>
-              <span>{(subtotal() + deliveryFee).toFixed(2)} USD</span>
-            </div>
-            <div className="cart-link">
-              <Link to={'/cart'}>Go to cart</Link>
-            </div>
-          </div>
-        </div>
-      </div>
     </div>
   );
 }
